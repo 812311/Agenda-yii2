318365face1648f85cefb12d35b2f921bcd476a4
Philipe <tui_hawk@hotmail.com>
2016-08-22 09:25:27 -0300

    error from remove grupos

--- views/site/error.php
+++ views/site/error.php
@@ -11,19 +11,31 @@ $this->title = $name;
 ?>
 <div class="site-error">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
+    <h1> <?=Html::encode("This group has at least one Contact")?>
+     </h1>
+    
     <div class="alert alert-danger">
-        <?=// if (preg_match('/foreign/',$message)) echo  "<br><b>Check if there is any Contact with this Group.</b>"
-            nl2br(Html::encode($message)) ?>
+        <?php 
+            if (substr_count((string)$name, 'Integrity constraint violation') > 0) {
+                
+                echo "<b>
+                        Check if there is any Contact with this Group, 
+                        remove the contact from the group and try delete the Group again.
+                     </b>";
+            
+            }else{
+               echo "<p>
+                        The above error occurred while the Web server was processing your request.
+                    </p>
+                    <p>
+                        Please contact us if you think this is a server error. Thank you.
+                    </p>";
+                    nl2br(Html::encode($message));
+            }     
+        ?>
        
     </div>
 
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
+    
 
 </div>
